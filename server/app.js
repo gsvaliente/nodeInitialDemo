e7edf44b3b4a2096edc2a9c79539a61f8605e876
Jess <jessicafala@gmail.com>
2022-10-24 12:51:56 +0200

    Created project structure folders

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+// TODO allow cors for localhost:3000 (la direcció del client)
+
+// TODO middlewares
+
+// TODO rutes
+
+// TODO sockets
+
+app.listen(port, () => {
+  console.log(`Xat server running on http://localhost:${port}`)
+})
