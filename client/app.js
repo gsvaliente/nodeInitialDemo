0e7a47d46001dc152f27782e229892ed24b0aefe
Jess <jessicafala@gmail.com>
2022-10-24 12:53:09 +0200

    done

---
+++ client/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/', express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Xat client running on http://localhost:${port}`)
+})
+
+/*
+const express = require('express'); // using express
+const socketIO = require('socket.io');
+const http = require('http');
+const port = process.env.PORT||3000 // setting the port
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+ 
+server.listen(port);
+*/
