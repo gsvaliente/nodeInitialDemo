f3ba7e225d065ff938489c14c5ec1e3a2cb4edbe
Jess <jessicafala@gmail.com>
2022-10-24 09:34:38 +0200

    Rectificaciones

--- Chat_client/config.js
+++ Chat_client/config.js
@@ -1,9 +1,9 @@
 const express = require('express'); // using express
 const socketIO = require('socket.io');
-const http = require('http')
+const http = require('http');
 const port = process.env.PORT||3000 // setting the port
-let app = express();
-let server = http.createServer(app)
-let io = socketIO(server)
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
  
 server.listen(port);
--- Chat_client/public/app.js
+++ Chat_client/public/app.js
@@ -3,11 +3,11 @@ const app = express()
     const port = 3000;
     app.use('/', express.static('public'));
 
-    // TODO allow cors for localhost 3000 (direccio del ciente)
+    // TODO allow cors for localhost 3000 (dirección del cliente)
     // TODO middlewares
     // TODO rutes
     // TODO sockets
 
     app.listen(port, ()=> {
-        console .log(`xat client runninf on http://localhost:${port}`);
+        console .log(`Xat client running on http://localhost:${port}`);
     })
