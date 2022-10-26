9e4668e638dd8bf8864ecf2e385489e3a332aca7
Jess <jessicafala@gmail.com>
2022-10-26 09:58:54 +0200

    minor changes

--- client/app.js
+++ client/app.js
@@ -6,9 +6,14 @@ let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 
 
-app.use('/', express.static('public'));
+//Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(express.static(path.join(__dirname, "public")));
+//route to index
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"./public/index.html"))
