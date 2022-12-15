e60b3f5ae844cdae1e55133131df1b8ed5143bd4
Jess <jessicafala@gmail.com>
2022-12-15 12:21:12 +0100

    files removed to improve structure

--- server/models/rooms.js
+++
@@ -1,9 +0,0 @@
-//TODO: crear rutas
-const { Router } = require("express");
-const router = Router();
-const userRoutes = require("./users");
-
-//rutas del usuario registro y login
-router.use("/users", userRoutes); 
-
-module.exports = router;
