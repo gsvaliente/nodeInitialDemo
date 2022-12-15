c6f6e1d635c7df9cabd4aa4d082471b2f9440d5c
Jess <jessicafala@gmail.com>
2022-12-15 12:18:06 +0100

    login&messages server part

---
+++ server/controllers/login.js
@@ -0,0 +1,48 @@
+const { Users } = require('../models/models.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const login = async (req, res) => {
+
+    const userName = req.body.userName;
+    const password = req.body.password;
+    
+    console.log('user input is :' + userName, password);
+
+    if(!userName || !password) {
+        return res.status(400).json({
+            status: 'error',
+            error: 'Username/password not provided'
+        });
+    }
+   
+    const userFound = await Users.findOne({ userName });
+
+    if(!userFound) {
+        return res.status(401).json({
+            status: 'error',
+            error: '401 - Unauthorized Access: Invalid username/password'
+        })
+    }
+
+    const validPassword = await bcrypt.compare(password, userFound.password);
+
+    if(validPassword) {
+            
+        const payload = {
+            userId: userFound._id,
+            userName: userFound.userName
+        }
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_KEY);
+        return res.status(200).json({accessToken: accessToken});
+    
+    } else {
+        return res.status(401).json({
+            status: 'error',
+            error: '401 - Unauthorized Access: Invalid username/password'
+        })
+    }
+ 
+}
+
+module.exports = login;
