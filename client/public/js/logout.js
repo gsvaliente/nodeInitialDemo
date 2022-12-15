c9653011b6f7166796769165f29e7027ca65b2df
Jess <jessicafala@gmail.com>
2022-12-15 12:03:55 +0100

    logout created

---
+++ client/public/js/logout.js
@@ -0,0 +1,7 @@
+document.querySelector('.button-logout').addEventListener('click', () => {
+    const leaveRoom = confirm('Are you sure that you want to log out?');
+    if(leaveRoom){
+        localStorage.clear();
+        window.location.replace('./login.html');
+    }
+})
