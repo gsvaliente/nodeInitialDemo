7bae0f1fba2a31d4e6c324c82de5b66d671a4de4
Jess <jessicafala@gmail.com>
2022-12-15 12:15:44 +0100

    Delete files to improve project structure

--- server/helpers/JWT.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const createJWT = async ( create ) => {
-    return jwt.sign( create, process.env.JWT_SECRET, {
-        expiresIn: "40d"
-    });
-}
-
-module.exports = { createJWT }
