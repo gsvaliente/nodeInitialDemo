08de0917389e6f482ef7478fe264386488dbb427
Jess <jessicafala@gmail.com>
2022-12-15 12:04:45 +0100

    main done

---
+++ client/public/js/main.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const accessToken = localStorage.getItem('accessToken');
+
+console.log(accessToken);
+
+if(!accessToken) {
+    window.location.assign('./login.html');
+}
+
+const socket = io('http://localhost:4001', {
+    reconnectionDelayMax: 10000,
+    query: {
+        'accessToken': localStorage.accessToken
+    }
+});
+
+/*const socket = io('http://localhost:4001', {
+    reconnectionDelayMax: 10000,
+    query: {
+        'accessToken': localStorage.accessToken
+
+        //'accessToken': myToken
+    }
+});*/
+
+
+
+/*
+const accessToken = localStorage.getItem('accessToken');
+
+console.log(accessToken);
+
+if(!accessToken) {
+    window.location.assign('./login.html');
+}
+
+const socket = io('http://localhost:4001', {
+    reconnectionDelayMax: 10000,
+    query: {
+        'accessToken': localStorage.accessToken
+
+        //'accessToken': myToken
+    }
+});
+
+*/
