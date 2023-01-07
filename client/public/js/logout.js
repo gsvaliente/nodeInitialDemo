7ccaf33f723722ee217ababea84f30c372ff1a57
Jess <jessicafala@gmail.com>
2023-01-07 21:11:15 +0100

    logout

--- client/public/js/logout.js
+++ client/public/js/logout.js
@@ -1,6 +1,7 @@
 document.querySelector('.button-logout').addEventListener('click', () => {
     const leaveRoom = confirm('Are you sure that you want to log out?');
     if(leaveRoom){
+        sessionStorage.clear();
         localStorage.clear();
         window.location.replace('./login.html');
     }
