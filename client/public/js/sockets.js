26fa16acc1ffe712fe80032c878cabd3ee69595b
Jess <jessicafala@gmail.com>
2023-01-07 21:17:53 +0100

    sockets/js/client finished

--- client/public/js/sockets.js
+++ client/public/js/sockets.js
@@ -1,38 +1,35 @@
-/*const socket = io('http://localhost:4001', {
-    reconnectionDelayMax: 10000,
-    query: {
-        'accessToken': localStorage.accessToken
-    }
-});*/
-
-//let socketConnected = false;
-
 socket.on('connect', () => {
+
+    socket.on('new-user', data => {
+        document.getElementById("userName").innerHTML = `Hi!${data.userName}`;
+        sessionStorage.setItem("userId",`${data.userId}`);
+        sessionStorage.setItem("userName",`${data.userName}`);
+    })
       
+     //Display new message in chat
+    socket.on('new-message', message => {
+        displayMessage(message);
+    })
+    
+    //Emit join message to chat
+    socket.on('new-join-message', message => {
+        displayJoinMessage(message);
+    })
+    
+    //New room created, updating user list
+    socket.on('new-room', (room, users) => {
+        displayRoom(room);
+        displayRoomUsers(room, users);
+    })
 
-    //display new message in chat
-   socket.on('new-message', message => {
-       displayMessage(message);
-   })
-   //emit join message to chat
-   socket.on('new-join-message', message => {
-       displayJoinMessage(message);
-   })
-   
-   //new room created, updating user list
-   socket.on('new-room', (room, users) => {
-       displayRoom(room);
-       displayRoomUsers(room, users);
-   })
+    socket.on('update-room-users', (room, users) => {
+        // Display updated list
+        if (sessionStorage.roomId === room.roomId) {
+            displayUsers(users)
+        }
+        displayRoomUsers(room, users);
+    })
 
-   socket.on('update-room-users', (room, users) => {
-       // Display updated list
-       if (sessionStorage.roomId === room.roomId) {
-           displayUsers(users)
-       }
-       displayRoomUsers(room, users);
-   })
-
-   // Retrieve room list 
-   socket.emit('get-rooms');
+    // Retrieve room list 
+    socket.emit('get-rooms');
 })
