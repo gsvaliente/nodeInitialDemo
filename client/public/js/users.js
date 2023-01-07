312668b4c4e98d22e4370477a716d0647caccff9
Jess <jessicafala@gmail.com>
2023-01-07 21:18:22 +0100

    users/js/client done

--- client/public/js/users.js
+++ client/public/js/users.js
@@ -1,15 +1,18 @@
-//creates list
+//Creates list
 const displayUsers = (users) => {
     const userList = document.getElementById("userList");
     userList.innerHTML = "";
-    // iterating the users array
+
+    // Iterating the users array
     users.forEach(user => {
-        //creating user 'li' element
+
+        //Creating user 'li' element
         const li = document.createElement('li');
         li.classList.add('user-li');
         li.textContent = user.userName;
         li.setAttribute("id", user.userId);
-        // append the user to the userList
+        
+        // Append the user to the userList
         userList.append(li);
     });
 }
