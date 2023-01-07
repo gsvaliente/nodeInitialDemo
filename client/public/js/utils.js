9fa9d05ce33892d6725931d3a9fb0da058f7d29c
Jess <jessicafala@gmail.com>
2023-01-07 21:18:50 +0100

    utils/js/client

--- client/public/js/utils.js
+++ client/public/js/utils.js
@@ -5,7 +5,19 @@ const sortBtnList = (btn) => {
     let limbo = buttonArray.shift(); 
     buttonList.append(limbo);
     buttonArray.sort((a, b) => {
-            return a.textContent.localeCompare(b.textContent)
+            return //Sort Limb to top and order buttonlist
+            const sortBtnList = (btn) => {
+                let buttonList = document.getElementById(btn);
+                let buttonArray = Array.from(buttonList.getElementsByTagName("BUTTON"))
+                let limbo = buttonArray.shift(); 
+                buttonList.append(limbo);
+                buttonArray.sort((a, b) => {
+                        return a.textContent.localeCompare(b.textContent)
+                    })
+                    .forEach(li => buttonList.append(li));
+            }
+            
+            a.textContent.localeCompare(b.textContent)
         })
         .forEach(li => buttonList.append(li));
 }
