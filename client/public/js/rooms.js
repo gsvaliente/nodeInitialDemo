945c4198dbf42a8a4d4a60a1a4a3c9342449dee6
Jess <jessicafala@gmail.com>
2023-01-07 21:17:15 +0100

    rooms/js/server

--- client/public/js/rooms.js
+++ client/public/js/rooms.js
@@ -1,24 +1,21 @@
-
-
 const joinRoom = (room) => {
     // Check if room other than current
     if (sessionStorage.roomId === room.roomId) return;
 
-     // emit to server 'join-room' event
+     // Emit to server 'join-room' event
     socket.emit('join-room', room);
 
     // Update variables
     sessionStorage.roomName = room.roomName;
     sessionStorage.roomId = room.roomId;
     
+    //Sets roomName
+    document.getElementById("roomName").innerHTML = `# ${room.roomName}`;
 
-    // Change room name
-    document.getElementById("roomName").innerHTML = `${room.roomName}`;
-
-   //clear messages from former room
+   //Clear messages from former room
     document.getElementById("messageList").innerHTML = "";
    
-    //clear forms
+    //Clear forms
     document.getElementById("roomForm").newRoom.value = "";
     document.getElementById("messageForm").newMessage.value = "";
     document.getElementById("newMessage").focus();
@@ -27,21 +24,19 @@ const joinRoom = (room) => {
 const displayRoom = (room) => {
 
     const btn = document.createElement('button');
-        if (room.roomName === 'Welcome!') {
+        if (room.roomName === 'Welcome') {
         btn.classList.add('room-btn-active');
         joinRoom(room);
-    } else {
-        btn.classList.add('room-btn');
-    }
+    } 
 
     btn.textContent = room.roomName;
     btn.setAttribute('id', room.roomId);
+    btn.classList.add('room-btn');
     btn.onclick = () => {
 
         if (sessionStorage.roomId) {
             document.getElementById(sessionStorage.roomId).classList.remove('room-btn-active')
         }
-
         btn.classList.add('room-btn-active');
         joinRoom(room);
 
@@ -54,7 +49,7 @@ const displayRoom = (room) => {
 }
 
 const displayRoomUsers = (room, users) => {
-    document.getElementById(room.roomId).textContent = `${room.roomName} (${users.length})`
+    document.getElementById(room.roomId).textContent = `${room.roomName} [Users:${users.length}]`
 }
 
 const createRoom = () => {
