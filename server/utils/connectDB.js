41d0ec32f7a9975cf45ad033ac621ce68b224b19
Jess <jessicafala@gmail.com>
2023-01-11 17:51:20 +0100

    cleaned code

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -1,6 +1,4 @@
-
 const mongoose = require('mongoose');
-
 
 module.exports = async () => {
     
@@ -8,7 +6,6 @@ module.exports = async () => {
     let mongoDB = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
     await mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
     console.log(`Connected to ${process.env.DB_NAME} DB`);
-    
 
     } catch (err) {
         console.log(err.message);
