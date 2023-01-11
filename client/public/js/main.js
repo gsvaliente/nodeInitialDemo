1478717ff740ca168eb067e7e5e6a26a7069dc03
Jess <jessicafala@gmail.com>
2023-01-11 09:44:16 +0100

    .

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -15,33 +15,3 @@ const socket = io('http://localhost:4001', {
     }
 });
 
-/*const socket = io('http://localhost:4001', {
-    reconnectionDelayMax: 10000,
-    query: {
-        'accessToken': localStorage.accessToken
-
-        //'accessToken': myToken
-    }
-});*/
-
-
-
-/*
-const accessToken = localStorage.getItem('accessToken');
-
-console.log(accessToken);
-
-if(!accessToken) {
-    window.location.assign('./login.html');
-}
-
-const socket = io('http://localhost:4001', {
-    reconnectionDelayMax: 10000,
-    query: {
-        'accessToken': localStorage.accessToken
-
-        //'accessToken': myToken
-    }
-});
-
-*/
