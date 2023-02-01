ff2dd09bd5901a19466428060e13d01e74595b30
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2023-02-01 14:35:15 +0100

    minor changes

--- app/server.js
+++ app/server.js
@@ -3,16 +3,15 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = require('./app');
-const sockets = require('./sockets/socket-back');
+const { listen } = require('./sockets/socket-back');
 
 const server = http.createServer(app);
 const io = new Server(server);
 
+listen(io);
 // io.on('connect', socketController);
 
 server.listen(
   process.env.PORT,
   console.log(`listening on port ${process.env.PORT}...`)
 );
-
-sockets.listen(io);
