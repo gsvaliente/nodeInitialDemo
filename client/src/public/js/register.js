4e0fedfa6e43b4c79acfc61c10a417d0e5e3e1bf
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2023-02-12 15:13:43 -0600

    register front end basic complete

---
+++ client/src/public/js/register.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('register-form');
+
+const registerUser = async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value;
+
+  if (!email || !password || !username) {
+    return alert('One of the fields is missing');
+  }
+
+  try {
+    const url = 'http://localhost:8080/api/v1/users/register';
+    const config = {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    };
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    if (data.success) {
+      localStorage.clear();
+      localStorage.setItem('accessToken', data.token);
+      localStorage.setItem('userID', data.user._id);
+      localStorage.setItem('username', data.user.username);
+      alert('registration complete');
+      window.location.assign('./chat.html');
+    } else {
+      console.log(data);
+      alert(data.msg.errors[0].msg);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+form.addEventListener('submit', registerUser);
