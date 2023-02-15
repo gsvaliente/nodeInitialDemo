00445ce2230fe54fbb676386f5e3f8c2056f0408
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2023-02-14 18:23:24 -0600

    another log

--- client/src/public/js/renders/users.js
+++ client/src/public/js/renders/users.js
@@ -3,7 +3,7 @@ socket.emit('onlineUsers');
 const userList = document.getElementById('user-list');
 socket.on('reloadUsers', (usersList) => {
   const { users } = usersList;
-  console.log(users);
+  // console.log(users);
 
   userList.innerHTML = `
     ${users.map((user) => `<li>${user.username}</li>`).join('')}
