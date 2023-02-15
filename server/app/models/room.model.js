6336dbb97c37928377cadad8f5d94f9e381c52cb
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2023-02-15 15:09:49 -0600

    minor changes

--- server/app/models/room.model.js
+++ server/app/models/room.model.js
@@ -1,24 +1,19 @@
 const { Schema, model } = require('mongoose');
 
-const RoomSchema = Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'field required'],
-      trim: true,
-      maxlength: 15,
-    },
-    messages: [
-      {
-        userData: { username: String, userID: String },
-        roomData: { roomN: String, roomID: String },
-        messageText: { type: String },
-      },
-    ],
+const RoomSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'field required'],
+    trim: true,
+    maxlength: 15,
   },
-  {
-    timestamps: true,
-  }
-);
+  messages: [
+    {
+      userData: { username: String, userID: String },
+      roomData: { roomN: String, roomID: String },
+      messageText: { type: String },
+    },
+  ],
+});
 
 module.exports = model('Room', RoomSchema);
